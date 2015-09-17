e7ac9c714eaefe682b3581f7cc825927124356e3
David House <Dahous1@umbc.edu>
2015-09-17 12:29:54 -0400

    Finished Project 14.1

---
+++ src/RandomCards.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by David on 9/17/2015.
+ * IS 413
+ * This Program Displays cards Randomly
+ * in a window
+ */
+public class RandomCards extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
