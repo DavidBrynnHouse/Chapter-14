b562736c00ae788d5b6e28943ebb29c2fa2a7fe0
David House <Dahous1@umbc.edu>
2015-09-17 11:26:27 -0400

    Initial commit

---
+++ src/Flags.java
@@ -0,0 +1,9 @@
+/**
+ * Created by David on 9/17/2015.
+ * IS 413
+ * This program displayes in a window the flags
+ * of the united states, united kingdom, china
+ * and canada
+ */
+public class Flags {
+}
