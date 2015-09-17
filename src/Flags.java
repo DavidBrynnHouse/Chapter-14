80e58bb4062a4a49195411f0cd3ccd8fdb00e864
David House <Dahous1@umbc.edu>
2015-09-17 12:26:24 -0400

    Finished Project 14.1

--- src/Flags.java
+++ src/Flags.java
@@ -1,9 +1,48 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
 /**
  * Created by David on 9/17/2015.
  * IS 413
- * This program displayes in a window the flags
+ * This program displays in a window the flags
  * of the united states, united kingdom, china
  * and canada
  */
-public class Flags {
+public class Flags extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.BASELINE_LEFT);
+        gridPane.setPadding(new Insets(5, 5, 5, 5));
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+
+        Image us = new Image("sample/us.gif");
+        Image uk = new Image("sample/uk.gif");
+        Image ca = new Image("sample/ca.gif");
+        Image china = new Image("sample/china.gif");
+
+        ImageView viewUs = new ImageView(us);
+        ImageView viewUk = new ImageView(uk);
+        ImageView viewCa = new ImageView(ca);
+        ImageView viewChina = new ImageView(china);
+
+        gridPane.add(viewCa, 0, 0);
+        gridPane.add(viewUs, 0, 1);
+        gridPane.add(viewUk, 1, 1);
+        gridPane.add(viewChina, 1, 0);
+
+
+        Scene scene = new Scene(gridPane, 400, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
