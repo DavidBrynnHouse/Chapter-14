be442fb10096aca30a072e7c3c2edcad24de5336
David House <Dahous1@umbc.edu>
2015-09-19 19:14:40 -0400

    Initialized project 14.5

--- src/FourFans.java
+++ src/FourFans.java
@@ -27,11 +27,10 @@ public class FourFans extends Application{
         Circle circle1 = new Circle(100);
         circle1.setStroke(Color.BLACK);
         circle1.setFill(Color.WHITE);
+
         Arc arc1 = new Arc();
         arc1.setFill(Color.RED);
         arc1.setType(ArcType.ROUND);
-        arc1.setRadiusX(100);
-        arc1.setRadiusY(100);
 
         Circle circle2 = new Circle(100);
         circle2.setStroke(Color.BLACK);
@@ -45,16 +44,15 @@ public class FourFans extends Application{
         circle4.setStroke(Color.BLACK);
         circle4.setFill(Color.WHITE);
 
+        pane.getChildren().add(arc1);
+
         pane.add(circle1, 0, 1);
-        pane.add(arc1, 0, 1);
         pane.add(circle2, 0, 0);
         pane.add(circle3, 1, 1);
         pane.add(circle4, 1, 0);
+
         Scene scene = new Scene(pane, 450, 450);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-}
-class Fan{
-        Fan(){}
 }
