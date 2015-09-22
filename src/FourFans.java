5abf28c7233adeae250609ba71e75ab3ea33648a
David House <Dahous1@umbc.edu>
2015-09-21 21:03:35 -0400

    Initialized project 14.5

--- src/FourFans.java
+++ src/FourFans.java
@@ -16,96 +16,99 @@ import javafx.stage.Stage;
 public class FourFans extends Application{
     @Override
     public void start(Stage primaryStage) {
-
         Pane pane = new Pane();
+        Color fan1Color = new Color(Math.random(), Math.random(), Math.random(), Math.random());
+        Color fan2Color = new Color(Math.random(), Math.random(), Math.random(), Math.random());
+        Color fan3Color = new Color(Math.random(), Math.random(), Math.random(), Math.random());
+        Color fan4Color = new Color(Math.random(), Math.random(), Math.random(), Math.random());
 
         Circle circle1 = new Circle(100);
-        circle1.setStroke(Color.BLACK);
+        circle1.setStroke(fan1Color);
         circle1.setFill(Color.WHITE);
         circle1.setCenterX(100);
         circle1.setCenterY(100);
 
         Arc arc11 = new Arc(100, 100, 80, 80, 30, 35);
-        arc11.setFill(Color.RED);
+        arc11.setFill(fan1Color);
         arc11.setType(ArcType.ROUND);
 
 
         Arc arc12 = new Arc(100, 100, 80, 80, 30 + 90, 35);
-        arc12.setFill(Color.RED);
+        arc12.setFill(fan1Color);
         arc12.setType(ArcType.ROUND);
 
         Arc arc13 = new Arc(100, 100, 80, 80, 30 + 180, 35);
-        arc13.setFill(Color.RED);
+        arc13.setFill(fan1Color);
         arc13.setType(ArcType.ROUND);
 
         Arc arc14 = new Arc(100, 100, 80, 80, 30 + 270, 35);
-        arc14.setFill(Color.RED);
+        arc14.setFill(fan1Color);
         arc14.setType(ArcType.ROUND);
 
         Circle circle2 = new Circle(100);
-        circle2.setStroke(Color.BLACK);
+        circle2.setStroke(fan2Color);
         circle2.setFill(Color.WHITE);
         circle2.setCenterX(325);
         circle2.setCenterY(100);
 
-        Arc arc21 = new Arc(100, 100, 80, 80, 30, 35);
-        arc21.setFill(Color.RED);
+        Arc arc21 = new Arc(325, 100, 80, 80, 30, 35);
+        arc21.setFill(fan2Color);
         arc21.setType(ArcType.ROUND);
 
-        Arc arc22 = new Arc(100, 100, 80, 80, 30, 35);
-        arc22.setFill(Color.RED);
+        Arc arc22 = new Arc(325, 100, 80, 80, 30 + 90, 35);
+        arc22.setFill(fan2Color);
         arc22.setType(ArcType.ROUND);
 
-        Arc arc23 = new Arc(100, 100, 80, 80, 30, 35);
-        arc23.setFill(Color.RED);
+        Arc arc23 = new Arc(325, 100, 80, 80, 30 + 180, 35);
+        arc23.setFill(fan2Color);
         arc23.setType(ArcType.ROUND);
 
-        Arc arc24 = new Arc();
-        arc24.setFill(Color.RED);
+        Arc arc24 = new Arc(325, 100, 80, 80, 30 + 270, 35);
+        arc24.setFill(fan2Color);
         arc24.setType(ArcType.ROUND);
 
         Circle circle3 = new Circle(100);
-        circle3.setStroke(Color.BLACK);
+        circle3.setStroke(fan3Color);
         circle3.setFill(Color.WHITE);
         circle3.setCenterX(100);
         circle3.setCenterY(325);
 
-        Arc arc31 = new Arc();
-        arc31.setFill(Color.RED);
+        Arc arc31 = new Arc(100, 325, 80, 80, 30, 35);
+        arc31.setFill(fan3Color);
         arc31.setType(ArcType.ROUND);
 
-        Arc arc32 = new Arc();
-        arc32.setFill(Color.RED);
+        Arc arc32 = new Arc(100, 325, 80, 80, 30 + 90, 35);
+        arc32.setFill(fan3Color);
         arc32.setType(ArcType.ROUND);
 
-        Arc arc33 = new Arc();
-        arc33.setFill(Color.RED);
+        Arc arc33 = new Arc(100, 325, 80, 80, 30 + 180, 35);
+        arc33.setFill(fan3Color);
         arc33.setType(ArcType.ROUND);
 
-        Arc arc34 = new Arc();
-        arc34.setFill(Color.RED);
+        Arc arc34 = new Arc(100, 325, 80, 80, 30 + 270, 35);
+        arc34.setFill(fan3Color);
         arc34.setType(ArcType.ROUND);
 
         Circle circle4 = new Circle(100);
-        circle4.setStroke(Color.BLACK);
+        circle4.setStroke(fan4Color);
         circle4.setFill(Color.WHITE);
         circle4.setCenterX(325);
         circle4.setCenterY(325);
 
-        Arc arc41 = new Arc();
-        arc41.setFill(Color.RED);
+        Arc arc41 = new Arc(325, 325, 80, 80, 30, 35);
+        arc41.setFill(fan4Color);
         arc41.setType(ArcType.ROUND);
 
-        Arc arc42 = new Arc();
-        arc42.setFill(Color.RED);
+        Arc arc42 = new Arc(325, 325, 80, 80, 30 + 90, 35);
+        arc42.setFill(fan4Color);
         arc42.setType(ArcType.ROUND);
 
-        Arc arc43 = new Arc();
-        arc43.setFill(Color.RED);
+        Arc arc43 = new Arc(325, 325, 80, 80, 30 + 180, 35);
+        arc43.setFill(fan4Color);
         arc43.setType(ArcType.ROUND);
 
-        Arc arc44 = new Arc();
-        arc44.setFill(Color.RED);
+        Arc arc44 = new Arc(325, 325, 80, 80, 30 + 270, 35);
+        arc44.setFill(fan4Color);
         arc44.setType(ArcType.ROUND);
 
         pane.getChildren().addAll(circle1, circle2, circle3,
